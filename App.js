b010031ab7b974e9a1c3afab812d4d0335ca4f8a
Eliel Fernandes Violante <167656404+DeEliel@users.noreply.github.com>
2024-11-28 22:17:34 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./App.css";
+import Header from "./cHeader.js";
+import Footer from "./cFooter.js";
+
+  function MeuPlano() {
+    return (
+      <main className="main-container">
+        <h1>Home</h1>
+        <div className="saldos">
+          <h2 className="entradas">Total Entradas: 6350</h2>
+          <h2 className="saidas">Total Saídas: 4140</h2>
+          <h2 className="saldo">Saldo Atual: 2210</h2>
+        </div>
+      </main>
+    );
+  }
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MeuPlano />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
